da0d1aa85933281a087de11cdcd9af772d2f1924
valourus <koen.vdels@xs4all.nl>
2018-06-08 20:47:38 +0100

    init commit

---
+++ Assets/PlayerConnection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnection : NetworkBehaviour 
+{
+	public GameObject playerObject;
+	
+	private void Start ()
+	{
+		if (isLocalPlayer)
+		{
+			CmdspawnPlayer();
+		}
+	}
+
+	[Command]
+	public void CmdspawnPlayer()
+	{
+		GameObject aPlayer = Instantiate(playerObject);
+		NetworkServer.SpawnWithClientAuthority(aPlayer, connectionToClient);
+	}
+	
+	void Update ()
+	{
+		if (!hasAuthority) return;
+		
+	}
+}
+ 
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+	private Rigidbody rb;
+	private float force { get; set; }
+	private float jumpForce { get; set; }
+	private bool isOnGround { get; set; }
+	
+	public void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = true;
+		force = 11.5f;
+		jumpForce = 10;
+		isOnGround = true;
+		rb.maxAngularVelocity = 2;
+	}
+	
+	public void FixedUpdate() {
+		rb.AddForce(Physics.gravity, ForceMode.Acceleration);
+	}
+	
+	private void Update()
+	{
+		if (!hasAuthority) return;
+		if (Input.GetKey(KeyCode.A))
+			rb.AddForce(Vector3.left * force);
+		if (Input.GetKey(KeyCode.D))
+			rb.AddForce(Vector3.right * force);
+		if (Input.GetKey(KeyCode.Space) && isOnGround)
+		{
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+			isOnGround = false;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+			rb.AddForce(Vector3.down * force);
+	}
+	
+	private void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag.Equals("ground"))
+			isOnGround = true;
+		else
+		{
+			Debug.Log("collosiondfieidsofj");
+			other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1500,( other.transform.position - transform.position )* 0.5F, 300, 300f);
+		}
+	}
+}
